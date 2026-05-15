df0cbd6a722c1ae36bee2750ce43f814e6f7a608
Hafsat Mukhtar <hafsatmukhtargaya@gmail.com>
2026-05-15 17:37:19 +0100

    Add new book button and dialog form

--- script.js
+++ script.js
@@ -15,6 +15,20 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read))
 }
 addBookToLibrary("Broken", "Fatima Bala", 300, true);
-addBookToLibrary("when the moon split", "Darrusalam", 300, true);
+addBookToLibrary("When the moon split", "Darrusalam", 300, true);
+addBookToLibrary("When the moon split", "Darrusalam", 300, true);
+addBookToLibrary("Time flies", "Edward Harry", 300, true);
 console.log(myLibrary);
 
+function renderLibrary() {
+    const container = document.getElementById("library-container");
+    container.innerHTML = "";
+    myLibrary.forEach(function(book) {
+        const card = document.createElement("div");
+        card.classList.add("book-card");
+        card.innerHTML = ` <p> ${book.title} </p> <p> ${book.author} </p> <p> ${book.pages} </p>`;
+        container.appendChild(card);
+    });
+}
+
+renderLibrary();
